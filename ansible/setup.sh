35f97d294730cf3c4efa7c7c2b3b51a29dcf4931
paul <paul.blouet@outlook.com>
2023-02-22 14:21:54 +0100

    fix: working on kuspray with private IPs

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -10,20 +10,24 @@ SSH_KEY_PATH="$HOME/.ssh/projetfinal"
 
 # get ips
 cd ../terraform
-K8S_MAIN_IP="`terraform output -raw k8s_main_ip`"
-K8S_WORKER_IP="`terraform output -raw k8s_worker_ip`"
+K8S_MAIN_PUBLIC_IP="`terraform output -raw k8s_main_public_ip`"
+K8S_MAIN_PRIVATE_IP="`terraform output -raw k8s_main_private_ip`"
+K8S_WORKER_PUBLIC_IP="`terraform output -raw k8s_worker_public_ip`"
+K8S_WORKER_PRIVATE_IP="`terraform output -raw k8s_worker_private_ip`"
 cd -
 
 # setup kubespray
 KUBESPRAY_PATH="kubespray-$KUBESPRAY_VERSION"
-[ -f "$KUBESPRAY_PATH" ] || \
-    curl -L "https://github.com/kubernetes-sigs/kubespray/archive/refs/tags/v$KUBESPRAY_VERSION.tar.gz" | \
+[ -d "$KUBESPRAY_PATH" ] && rm -rf "$KUBESPRAY_PATH"
+curl -L "https://github.com/kubernetes-sigs/kubespray/archive/refs/tags/v$KUBESPRAY_VERSION.tar.gz" | \
     tar xz
 cd "$KUBESPRAY_PATH"
 python -m venv .env
 source .env/bin/activate
 pip install -r requirements.txt
 pip install -r contrib/inventory_builder/requirements
-python contrib/inventory_builder/inventory.py "$K8S_MAIN_IP" "$K8S_WORKER_IP"
-ansible-playbook -i inventory/sample/inventory.ini cluster.yml -b -v \
-    --private-key="$SSH_KEY_PATH"
+python contrib/inventory_builder/inventory.py \
+    "$K8S_MAIN_PRIVATE_IP,$K8S_MAIN_PUBLIC_IP" \
+    "$K8S_WORKER_PRIVATE_IP,$K8S_WORKER_PUBLIC_IP"
+ansible-playbook -i inventory/sample/hosts.yaml cluster.yml -b -v \
+    --private-key="$SSH_KEY_PATH" --user adminuser
