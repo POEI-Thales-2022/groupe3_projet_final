d27e59bd3fa03f848e2006a71e5765368131823e
paul <paul.blouet@outlook.com>
2023-02-22 10:33:03 +0100

    feat: add vnet to tf
    Co-authored-by: SabrinaMacaluso <SabrinaMacaluso@users.noreply.github.com>
    Co-authored-by: MarconiMat <MarconiMat@users.noreply.github.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,17 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name     = random_pet.rg_name.id
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet"
+  resource_group_name = random_pet.rg_name.id
+  location            = var.resource_group_location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet"
+  resource_group_name  = random_pet.rg_name.id
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
