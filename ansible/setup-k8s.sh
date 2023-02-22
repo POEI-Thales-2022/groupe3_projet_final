efb1b814f9a44cd0443ed7692d144139551ca24f
paul <paul.blouet@outlook.com>
2023-02-22 16:07:10 +0100

    fix: wip simpler k8s

--- ansible/setup-k8s.sh
+++ ansible/setup-k8s.sh
@@ -12,8 +12,6 @@ SSH_KEY_PATH="$HOME/.ssh/projetfinal"
 cd ../terraform
 K8S_MAIN_PUBLIC_IP="`terraform output -raw k8s_main_public_ip`"
 K8S_MAIN_PRIVATE_IP="`terraform output -raw k8s_main_private_ip`"
-K8S_WORKER_PUBLIC_IP="`terraform output -raw k8s_worker_public_ip`"
-K8S_WORKER_PRIVATE_IP="`terraform output -raw k8s_worker_private_ip`"
 cd -
 
 # setup kubespray
@@ -26,7 +24,6 @@ python -m venv .env
 source .env/bin/activate
 pip install -r requirements.txt
 python contrib/inventory_builder/inventory.py \
-    "$K8S_MAIN_PRIVATE_IP,$K8S_MAIN_PUBLIC_IP" \
-    "$K8S_WORKER_PRIVATE_IP,$K8S_WORKER_PUBLIC_IP"
+    "$K8S_MAIN_PRIVATE_IP,$K8S_MAIN_PUBLIC_IP"
 ansible-playbook -i inventory/sample/hosts.yaml cluster.yml -b -v \
     --private-key="$SSH_KEY_PATH" --user adminuser
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -14,14 +14,6 @@ output "k8s_main_private_ip" {
   value = azurerm_network_interface.k8s_main_nic.private_ip_address
 }
 
-output "k8s_worker_public_ip" {
-  value = azurerm_public_ip.k8s_worker_ip.ip_address
-}
-
-output "k8s_worker_private_ip" {
-  value = azurerm_network_interface.k8s_worker_nic.private_ip_address
-}
-
 output "iscsi_ip" {
   value = azurerm_public_ip.iscsi_ip.ip_address
 }
