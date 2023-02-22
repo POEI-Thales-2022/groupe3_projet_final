1f53c8a3c37f6754ebcc717a5f142ee74a6173a0
paul <paul.blouet@outlook.com>
2023-02-22 14:33:36 +0100

    fix: add icmp rule for ansible

--- terraform/k8s-worker.tf
+++ terraform/k8s-worker.tf
@@ -5,48 +5,6 @@ resource "azurerm_public_ip" "k8s_worker_ip" {
   allocation_method   = "Static"
 }
 
-resource "azurerm_network_security_group" "k8s_worker_nsg" {
-  name                = "k8s-worker-nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "HTTPS"
-    priority                   = 1003
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "443"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "HTTP"
-    priority                   = 1004
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
 resource "azurerm_network_interface" "k8s_worker_nic" {
   name                = "k8s-worker-nic"
   location            = azurerm_resource_group.rg.location
@@ -62,7 +20,7 @@ resource "azurerm_network_interface" "k8s_worker_nic" {
 
 resource "azurerm_network_interface_security_group_association" "k8s_worker_nsg_assoc" {
   network_interface_id      = azurerm_network_interface.k8s_worker_nic.id
-  network_security_group_id = azurerm_network_security_group.k8s_worker_nsg.id
+  network_security_group_id = azurerm_network_security_group.k8s_nsg.id
 }
 
 resource "azurerm_linux_virtual_machine" "k8s_worker" {
