3ae88fe3147715834f6e68dc224c3005437f06cd
paul <paul.blouet@outlook.com>
2023-02-22 10:40:23 +0100

    feat: tf vm

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,3 +20,24 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.0.0/24"]
 }
+
+#creation vm
+resource "azurerm_linux_virtual_machine" "gitlab" {
+  name                = "gitlab"
+  resource_group_name = random_pet.rg_name.id
+  location            = var.resource_group_location
+  size                = "Standard_B4msv2"
+  admin_username      = "adminuser"
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/projetfinal.pub")
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "22.04-LTS"
+    version   = "latest"
+  }
+}
