e56877b1a3e5517d734d0a686ba540d0940c631c
root <you@example.com>
2023-02-23 17:09:10 +0100

    ISCSI test

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,10 @@
 variable "resource_group_location" {
-  default     = "westus"
+  default     = "westus3"
   description = "Location of the resource group."
 }
 
 variable "resource_group_name" {
-  default = "projet-test"
+  default = "projet-final-groupe-3"
 }
 
 variable "image_publisher" {
